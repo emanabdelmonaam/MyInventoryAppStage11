526920f1c0685dcce906210140f64ace6878a16c
emanabdelmonaam <emanabdelmonaam@gmail.com>
2018-06-10 04:09:47 +0200

    One

--- app/src/main/java/com/example/android/myinventoryappstage11/data/BookDbHelper.java
+++ app/src/main/java/com/example/android/myinventoryappstage11/data/BookDbHelper.java
@@ -40,7 +40,7 @@ public class BookDbHelper extends SQLiteOpenHelper {
         String SQL_CREATE_BOOKS_TABLE =  "CREATE TABLE " + BookContract.BookEntry.TABLE_NAME +"("
                 + BookEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + BookEntry.COLUMN_BOOK_NAME + "TEXT NOT NULL, "
-                + BookEntry.COLUMN_BOOK_PRICE + "INTEGER NOT NULL, "
+                + BookEntry.COLUMN_BOOK_PRICE + "INTEGER NOT NULL DEFAULT 0, "
                 + BookEntry.COLUMN_BOOK_QUANTITY + "INTEGER NOT NULL DEFAULT 0,"
                 + BookEntry.COLUMN_BOOK_TYPE + "INTEGER NOT NULL, "
                 + BookEntry.COLUMN_BOOK_SUPPLIER_NAME + "TEXT NOT NULL,"
